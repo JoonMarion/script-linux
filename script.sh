28f11950dc020e79c4da11d4393bfa5d1058f390
João Mariano <84255348+JoonMarion@users.noreply.github.com>
2022-10-15 10:00:19 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Criando diretórios..."
+echo "Criando diretorios..."
 
 mkdir /publico
 mkdir /adm
@@ -11,14 +11,14 @@ echo "Criando grupos..."
 groupadd GRP_ADM
 groupadd GRP_VEN
 
-echo "Criando usuários no sistema..."
+echo "Criando usuarios no sistema..."
 
-useradd joao -c "João Mariano" -m -s /bin/bash -p $(openssl passwd -crypt senhadojoao) -G GRP_ADM
-useradd pedro -c "Pedro da Silva" -m -s /bin/bash -p $(openssl passwd -crypt senhadopedro) -G GRP_ADM
-useradd melissa -c "Melissa Hapuq" -m -s /bin/bash -p $(openssl passwd -crypt senhadamelissa) -G GRP_VEN
-useradd leticia -c "Letícia Paiva" -m -s /bin/bash -p $(openssl passwd -crypt senhadaleticia) -G GRP_VEN
+useradd joao -c "Joao Mariano" -m -s /bin/bash -p $(openssl passwd senhadojoao) -G GRP_ADM
+useradd pedro -c "Pedro da Silva" -m -s /bin/bash -p $(openssl passwd senhadopedro) -G GRP_ADM
+useradd melissa -c "Melissa Hapuq" -m -s /bin/bash -p $(openssl passwd senhadamelissa) -G GRP_VEN
+useradd leticia -c "Letícia Paiva" -m -s /bin/bash -p $(openssl passwd senhadaleticia) -G GRP_VEN
 
-echo "Adicionando permissões aos diretórios ..."
+echo "Adicionando permissoes aos diretorios ..."
 
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
