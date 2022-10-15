af089b8994aa8e30524d682e663b7f4d273fae18
João Mariano <84255348+JoonMarion@users.noreply.github.com>
2022-10-15 10:00:40 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,7 +16,7 @@ echo "Criando usuarios no sistema..."
 useradd joao -c "Joao Mariano" -m -s /bin/bash -p $(openssl passwd senhadojoao) -G GRP_ADM
 useradd pedro -c "Pedro da Silva" -m -s /bin/bash -p $(openssl passwd senhadopedro) -G GRP_ADM
 useradd melissa -c "Melissa Hapuq" -m -s /bin/bash -p $(openssl passwd senhadamelissa) -G GRP_VEN
-useradd leticia -c "Letícia Paiva" -m -s /bin/bash -p $(openssl passwd senhadaleticia) -G GRP_VEN
+useradd leticia -c "Leticia Paiva" -m -s /bin/bash -p $(openssl passwd senhadaleticia) -G GRP_VEN
 
 echo "Adicionando permissoes aos diretorios ..."
 
